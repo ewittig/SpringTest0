7016665008ed8f7dc2aad3cc7286bc2f54e08f85
Elizabeth Plumb <elizabeth.plumb@datadoghq.com>
2019-04-25 14:20:34 -0700

    initial commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,30 +19,26 @@ import java.util.Map;
 @RestController
 public class GreetingController {
 
-
     @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
     HttpServletRequest request;
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @RequestMapping("/ServiceC")
     public String serviceC() throws InterruptedException {
-
         // Hashmap containing Header key/val
         Map<String, String> map = new HashMap<>();
-
         //build HttpHeader
         HttpHeaders header = new HttpHeaders();
         header.setAll(map);
-
         //Sleep
         Thread.sleep(250L);
-
+        logger.info("In Service C ***************");
         //Post to downstream service
         String rs = restTemplate.postForEntity("http://localhost:9393/ServiceD", new HttpEntity(header), String.class).getBody();
-
         return rs;
     }
 
@@ -59,6 +57,7 @@ public class GreetingController {
         }
 
         Thread.sleep(230L);
+        logger.info("In Service D ***************");
 
         return "Service D\n";
     }
